b8b987bb435f39ebd480f14be77ee79ea8eb5f65
louiseroo <louise.nina.roo@gmail.com>
2023-03-22 11:33:27 +0100

    debut interface

--- typotheque-prevert/user/themes/typotheque-prevert/js/functions.js
+++ typotheque-prevert/user/themes/typotheque-prevert/js/functions.js
@@ -1,3 +1,9 @@
 $(function() {
-
+	function getRandom(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	$("h1").each(function(){
+		$(this).css('left', getRandom(0, 1000)+"px");
+	});
 });
+
